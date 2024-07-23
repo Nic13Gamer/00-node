b665e90789a8153ee4df03428c5fa712944176ef
Levi <levi@technomancy.dev>
2024-07-23 06:28:11 +0200

    feat(broadcasts): function & types for api
    This adds the broadcast method and its types.

--- src/emails/emails.ts
+++ src/emails/emails.ts
@@ -1,11 +1,20 @@
 import { DoubleZero } from '../core';
-import { CreateEmailParams } from './types';
+import { CreateBroadcastParams, CreateEmailParams } from './types';
 
 export class Emails {
   constructor(private readonly doublezero: DoubleZero) {}
 
   async send(params: CreateEmailParams): Promise<{ id: string }> {
     const data = await this.doublezero.post<{ id: string }>('/emails', params);
+
+    return data;
+  }
+
+  async broadcast(params: CreateBroadcastParams): Promise<{ id: string }> {
+    const data = await this.doublezero.post<{ id: string }>(
+      '/emails/broadcasts/',
+      params
+    );
 
     return data;
   }
